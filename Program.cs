42c5993513fb97b4997813c7cab08e151f51d570
seal031 <87749356@qq.com>
2019-05-24 17:28:23 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTrainning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strPath = "c2ltczovLy8xMDAwMSxzaW1zLDE1MzE5MjUwMzcyMDcsNGI0ZTRkZTE4NTVmOWMzYWRjNDUyNjE4MmE3NGQzYmE = ";
+            byte[] outputb = Convert.FromBase64String(strPath);
+            strPath = Encoding.Default.GetString(outputb);
+        }
+    }
+}
